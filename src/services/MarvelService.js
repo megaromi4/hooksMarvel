4d7a89bf0378b3378eb3a5da6bed7eaa8928a879
Your NameRoman <rabcevra@gmail.com>
2023-01-15 22:58:26 +0300

    Start marvel App

---
+++ src/services/MarvelService.js
@@ -0,0 +1,16 @@
+class MarvelService {
+  getResource = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    return this.getResource("");
+  };
+}
+
+export default MarvelService;
