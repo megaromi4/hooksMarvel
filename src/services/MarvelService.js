9b57ec85cd929b49633b92d8db8e54017e55ada5
Your NameRoman <rabcevra@gmail.com>
2023-03-19 18:59:59 +0300

    add random char

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,6 @@
 class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=c49f3da2ea87fea65248d20d1356f362";
   getResource = async (url) => {
     let res = await fetch(url);
     if (!res.ok) {
@@ -8,8 +10,46 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource("");
+  getAllCharacters = async () => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter());
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  _transformCharacter = (char) => {
+    if (!char.description) {
+      return {
+        name: char.name,
+        description: "No description",
+        thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url,
+      };
+    }
+    if (char.description.length > 211) {
+      return {
+        name: char.name,
+        description: char.description.slice(0, 211) + "...",
+        thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+        homepage: char.urls[0].url,
+        wiki: char.urls[1].url,
+      };
+    }
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
