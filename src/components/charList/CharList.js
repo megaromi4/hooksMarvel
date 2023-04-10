920b0e7605cf4b5ed6eaef6360b3b8028324b0cc
Your NameRoman <rabcevra@gmail.com>
2023-04-10 22:34:21 +0300

    add max load new items

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -15,6 +15,7 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     offset: 210,
+    charEnded: false,
   };
 
   componentDidMount() {
@@ -36,11 +37,16 @@ class CharList extends Component {
   };
 
   onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
     this.setState(({ offset, charList }) => ({
       charList: [...charList, ...newCharList],
       loading: false,
       newItemLoading: false,
       offset: offset + 9,
+      charEnded: ended,
     }));
   };
 
@@ -52,7 +58,8 @@ class CharList extends Component {
   };
 
   render() {
-    const { charList, loading, error, offset, newItemLoading } = this.state;
+    const { charList, loading, error, offset, newItemLoading, charEnded } =
+      this.state;
 
     const items = charList.map((char) => {
       return (
@@ -83,6 +90,7 @@ class CharList extends Component {
         <button
           className="button button__main button__long"
           disabled={newItemLoading}
+          style={{ display: charEnded ? "none" : "block" }}
           onClick={() => {
             this.onRequest(offset);
           }}
